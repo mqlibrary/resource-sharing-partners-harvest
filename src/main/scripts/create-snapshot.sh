fb8ece5ea694a5949322021318d9c9fcb189471b
nishen <nish.naidoo@gmail.com>
2019-09-22 11:22:17 +1000

    Updated script.

--- src/main/scripts/create-snapshot.sh
+++ src/main/scripts/create-snapshot.sh
@@ -2,15 +2,14 @@
 
 CURRENT_TIME=`date +'%Y%m%d%H%M%S'`
 
-ES_CERT=/path/to/cert
 ES_USER=username
 ES_PASS=password
-ES_BASE=http://elastic.svc.library.local/
-ES_REPO=mqlibraryes-snapshot-repo
+ES_BASE=http://elastic.svc.lib.mq.edu.au
+ES_REPO=backups
 ES_SNAP=snap-$CURRENT_TIME
 
 ES_DATA="{ \"indices\": \"partner-*\", \"ignore_unavailable\": true, \"include_global_state\": false }"
 
 URL="$ES_BASE/_snapshot/$ES_REPO/$ES_SNAP" 
 
-echo $ES_DATA | http --auth "$ES_USER:$ES_PASS" --verify=$ES_CERT PUT $URL
+curl -s --user $ES_USER:$ES_PASS -XPUT "$URL" -H "Content-Type: application/json" -H "Accept: application/json" -d $ES_DATA
